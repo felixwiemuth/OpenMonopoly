0071d85b0419dfedfa19f0f263624a1133a2a53c
Felix Wiemuth <felixwiemuth@hotmail.de>
2012-02-23 19:25:51 +0100

    class representing a player

---
+++ src/backend/Player.java
@@ -0,0 +1,30 @@
+/******************************************************
+ * Copyright (C) 2012 Felix Wiemuth                   *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE      *
+ * See LICENSE or http://www.gnu.org/licenses/gpl.txt *
+ ******************************************************/
+
+package backend;
+
+import backend.core.PlanElement;
+import java.util.ArrayList;
+
+/**
+ * The OpenMonopoly player.
+ *
+ * @author Felix Wiemuth
+ */
+public class Player {
+
+    private String name;
+    private int money;
+    private ArrayList<PlanElement> property;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String name() {
+        return name;
+    }
+}
