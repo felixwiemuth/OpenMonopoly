9447d581b76ee2c2af58a7f315e7d52d0b84b791
Felix Wiemuth <felixwiemuth@hotmail.de>
2012-03-20 12:38:35 +0100

    pre structure of renderer interface

--- src/frontend/Renderer.java
+++ src/frontend/Renderer.java
@@ -6,11 +6,22 @@
 
 package frontend;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 /**
  * Interface for all types of renderers (2D, 3D).
  * OpenMonopoly uses the selected renderer over this interface.
  * @author Felix Wiemuth
  */
-public interface Renderer {
+public abstract class Renderer {
+    private ConcurrentLinkedQueue<String> animationPening;
+    
+    public enum Animation {
+        MOVE_PLAYER_FORWARDS,
+        MOVE_PLAYER_BACKWARDS;
+    }
+    
+    //TODO different types of data must be passed to animate...
+    public abstract void animate(Animation a);
 
 }
