39daf4f687a5ba9bff4604f1e30b240878edfc43
Felix Wiemuth <felixwiemuth@hotmail.de>
2012-04-09 17:57:57 +0200

    pre structure of UI code interface

---
+++ src/frontend/UI.java
@@ -0,0 +1,17 @@
+/******************************************************
+ * Copyright (C) 2012 Felix Wiemuth                   *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE      *
+ * See LICENSE or http://www.gnu.org/licenses/gpl.txt *
+ ******************************************************/
+
+package frontend;
+
+/**
+ * Interface for different UIs (GUI, CUI, ...)
+ * @author felix
+ */
+
+
+public interface UI {
+    
+}
