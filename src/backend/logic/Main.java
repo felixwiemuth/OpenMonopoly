8cb8539fbeb5360f5ee45dc7aec6510da5914a0c
Felix Wiemuth <felixwiemuth@hotmail.de>
2012-04-09 15:44:24 +0200

    main logic functions used by modules

---
+++ src/backend/logic/Main.java
@@ -0,0 +1,22 @@
+/******************************************************
+ * Copyright (C) 2012 Felix Wiemuth                   *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE      *
+ * See LICENSE or http://www.gnu.org/licenses/gpl.txt *
+ ******************************************************/
+
+package backend.logic;
+
+import backend.Player;
+
+/**
+ *
+ * @author felix
+ */
+
+
+public class Main {
+    public static void move_player(Player player, int dist) {
+        //TODO implement
+    }
+    
+}
